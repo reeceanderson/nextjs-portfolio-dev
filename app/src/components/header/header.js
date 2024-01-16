cdc529ee1008c72c76a00f8be514c8a90b4d3541
Reece Anderson <reeceandersonnn@gmail.com>
2024-01-16 17:14:56 +0000

    Tinkering

--- app/src/components/header/header.js
+++ app/src/components/header/header.js
@@ -18,12 +18,12 @@ const Header = () => {
             <motion.path class="lines" d="M328.56,332.65H247.77V599.8h69.57V444.63h0L403.27,599.8H498C461.71,542.31,364.11,387.46,328.56,332.65Z"
               initial={{ pathLength: 0 }}
               animate={{ pathLength: 1 }}
-              transition={{ repeatType: "reverse", repeat: Infinity, duration: 3, repeatDelay: 2, easings: 'easeInOut' }}
+              transition={{ repeatType: "reverse", repeat: Infinity, duration: 3, repeatDelay: 2, ease: 'easeInOut' }}
             />
             <motion.path class="lines" d="M387.17,294c57.89-.07,71.37,25.34,71.37,56.15,0,26-19.47,47.84-45.82,54l46,64.27c107.35-24.39,123-174.67,43.19-239.22C475,208.46,441.23,202,408.05,200.8c-22.75-1-135.48-.39-160.28-.55l.38,93.78h139"
               initial={{ pathLength: 0 }}
               animate={{ pathLength: 1 }}
-              transition={{ repeatType: "reverse", repeat: Infinity, duration: 3, repeatDelay: 2, easings: 'easeInOut' }}
+              transition={{ repeatType: "reverse", repeat: Infinity, duration: 3, repeatDelay: 2, ease: 'easeInOut' }}
             />
           </g>
         </svg>
--- app/src/components/hero/hero.js
+++ app/src/components/hero/hero.js
@@ -4,11 +4,14 @@ import { motion } from "framer-motion";
 
 const Hero = () => {
     return(
-      <div className='flex justify-center items-center h-96'>
-        <motion.h1 className='text-5xl'
-          whileHover={{ scale: 1.9 }}
-          transition={{ duration: 0.5 }}
-        >Hero</motion.h1>
+      <div className='grid justify-center items-center h-96 w-screen'>
+        <div className=''>
+          <h1 className='text-4xl text-center'>hi, i'm reece anderson</h1>
+        </div>
+        <br />
+        <div className=''>
+          <h3 className='text-lg text-center'>an IT student passionate about computing security and systems administration. </h3>
+        </div>
       </div>
     );
   };
